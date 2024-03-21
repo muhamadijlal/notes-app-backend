2f2430be98d61cf260cd206ac6f9d41bb2422241
muhamadijlal <haidarijlal027@gmail.com>
2024-03-21 09:17:51 +0700

    initial commit

---
+++ src/notes.js
@@ -0,0 +1,12 @@
+const notes = [
+  {
+    id: 'bdFhuRMNWvI3x2mT',
+    title: 'Pemrograman',
+    createdAt: '2024-03-20T13:39:28.625Z',
+    updatedAt: '2024-03-20T13:39:28.625Z',
+    tags: ['Laravel', 'ReactJS', 'NodeJS'],
+    body: 'Belajar pemrograman bersama dicoding!'
+  }
+]
+
+module.exports = notes
